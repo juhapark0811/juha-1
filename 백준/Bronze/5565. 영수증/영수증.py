4663e8c340830da754d79c06a880ab8189caa09f
juhapark0811 <juhaparkclare0811@gmail.com>
2026-01-01 16:20:15 +0900

    [Bronze IV] Title: 영수증, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = 0
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+g = int(input())
+h = int(input())
+j = int(input())
+k = int(input())
+m = int(input())
+for i in range(1, 10):
+        b = a - (c+d+e+f+g+h+j+k+m)
+print(b)
